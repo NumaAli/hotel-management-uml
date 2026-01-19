1d647f75193d127ff730efe5e035b90671e3b812
Numa Ali <numarahim62@gmail.com>
2026-01-19 21:38:24 +0500

    feat: add Name value object

---
+++ hotel-management/src/main/java/com/numa/hotel/valueobject/Name.java
@@ -0,0 +1,19 @@
+package com.numa.hotel.valueobject;
+
+public class Name {
+    
+    private final String value;
+
+    public Name(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
+
+    
